6a166d346b567403244cd7d17afd002c266ba770
VyseExhale <vyseexhale@gmail.com>
2014-02-26 11:05:50 +0100

    cSimpleXMLElement added method loadStringToArray

--- cSimpleXMLElement.php
+++ cSimpleXMLElement.php
@@ -4,7 +4,6 @@
 	 * 	Contains less common SimipleXMLElement functions 
 	 *	@version 1.0
 	 * 	@author Barry Dam
-	 * 	@see http://www.php.net/manual/en/class.simplexmlelement.php
 	 */
 	class cSimpleXMLElement extends \SimpleXMLElement {
 		/**
@@ -12,30 +11,21 @@
 		 * @param (SimpleXMLElement) $getXml
 		 */
 		public static function convertToArray($getXml = false) 
-		{
-			$objChildren = false;
-			$return 	 = null;
+		{	
 			if ($getXml instanceof SimpleXMLElement || $getXml instanceof cSimpleXMLElement) {
-				$objChildren = $getXml->children();
-			}
-			if (! $objChildren) return false;
-			foreach ($objChildren as $key => $value) {
-				$arrValues = (array)$value->children();
-				if (count($arrValues) > 0) {
-					$return[$key] = self::convertToArray($value);
-				} else {
-					if (! isset($return[$key])) {
-						$return[$key] = (string)$value;
-					} else {
-						if (! is_array($return[$key])) {
-							$return[$key] = array($return[$key], (string)$value);
-						} else {
-							$return[$key][] = (string)$value;
-						}
-					}
-				}
-			}
-			return $return;
+				$json = json_encode($getXml);
+				return json_decode($json, true);
+			} 
+		}
+		/**
+		 * Convert a xml string to array
+		 * @param (string) xml
+		 */
+		public static function loadStringToArray($getString = false)
+		{
+			if (! is_string($getString)) return false;
+			$objSimpleXML = simplexml_load_string($getString);
+			if ($objSimpleXML) return self::convertToArray($objSimpleXML);
 		}
 	};
 ?>
