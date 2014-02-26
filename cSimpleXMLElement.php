fbbefa9309eadfa5742bad0cc35c6d61c6d3438d
VyseExhale <vyseexhale@gmail.com>
2014-02-26 10:19:09 +0100

    Added new custom class cSimpleXMLElement with firtst method convertToArray

---
+++ cSimpleXMLElement.php
@@ -0,0 +1,40 @@
+<?php
+	/**
+	 *	Custom SimipleXMLElement class
+	 * 	Contains less common SimipleXMLElement functions 
+	 *	@version 1.0
+	 * 	@author Barry Dam
+	 */
+	class cSimpleXMLElement extends \SimpleXMLElement {
+		/**
+		 * Convert an SimpleXMLElement object to array
+		 * @param (SimpleXMLElement) $getXml
+		 */
+		public static function convertToArray($getXml = false) 
+		{
+			$objChildren = false;
+			$return 	 = null;
+			if ($getXml instanceof SimpleXMLElement || $getXml instanceof cSimpleXMLElement) {
+				$objChildren = $getXml->children();
+			}
+			if (! $objChildren) return false;
+			foreach ($objChildren as $key => $value) {
+				$arrValues = (array)$value->children();
+				if (count($arrValues) > 0) {
+					$return[$key] = self::convertToArray($value);
+				} else {
+					if (! isset($return[$key])) {
+						$return[$key] = (string)$value;
+					} else {
+						if (! is_array($return[$key])) {
+							$return[$key] = array($return[$key], (string)$value);
+						} else {
+							$return[$key][] = (string)$value;
+						}
+					}
+				}
+			}
+			return $return;
+		}
+	};
+?>
