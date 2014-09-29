422c3a30b618a811d165957059e146e88d017642
Barry Dam <barry@bicmultimedia.nl>
2014-09-29 14:17:18 +0200

    Added truncate method to cString

--- cString.php
+++ cString.php
@@ -1,9 +1,11 @@
 <?php
 	/**
-	 * cString contains string functions which are often needed
+	 * cString contains string functions which are often used
 	 */
 	class cString {
 		private $str = false;
+
+		// Magic Methods //
 
 		public function __construct($str) 
 		{
@@ -14,12 +16,38 @@
 			return $this->str;
 		}
 
-		public function trim($getCharlist)
+		// existing string methods (mostly needed in Extra added methods)
+
+		public function base64decode()
 		{
-			$str = trim($this->str, $getCharlist);
+			$str = base64_decode($this->str);
 			return new cString($str);
 		}
-
+		public function base64encode()
+		{
+			$str = base64_encode($this->str);
+			return new cString($str);
+		}
+		public function gzdeflate($getIntLevel = -1, $getIntEncoding = ZLIB_ENCODING_RAW)
+		{
+			$str = gzdeflate($this->str, $getIntLevel, $getIntEncoding);
+			return new cString($str);
+		}
+		public function gzinflate($getIntLength = 0)
+		{
+			$str = gzinflate($this->str, $getIntLength);
+			return new cString($str);
+		}
+		public function rot13()
+		{
+			$str = str_rot13($this->str);
+			return new cString($str);
+		}
+		public function stripTags() 
+		{
+			$str = strip_tags($this->str);
+			return new cString($str);
+		}
 		public function toLowerCase() 
 		{
 			$str = strtolower($this->str);
@@ -30,6 +58,13 @@
 			$str = strtoupper($this->str);
 			return new cString($str);
 		}
+		public function trim($getCharlist)
+		{
+			$str = trim($this->str, $getCharlist);
+			return new cString($str);
+		}
+
+		// Extra added methods
 
 		/**
 		 * do a replace or preg_replace
@@ -48,13 +83,12 @@
 			}
 			return new cString($str);
 		}
-
-		public function stripTags() 
-		{
-			$str = strip_tags($this->str);
-			return new cString($str);
-		}
-
+		/**
+		 * format string to a valid ASCII string
+		 * @param  mixed or array  $getReplace  all chars passed will be replaced by $getDelimiter    
+		 * @param  string $getDelimiter  all whitespaces will be replaced by this 
+		 * @return cString
+		 */
 		public function toASCII($getReplace = array(), $getDelimiter = '-')
 		{
 			$str = $this->str;
@@ -71,14 +105,13 @@
 				->replace(' ', $getDelimiter);
 			return new cString($str);
 		}
-
 		/**
 		 * format a string to url-safe string
 		 * example : 
 		 * This Is the page
 		 * will be formatted to:
 		 * this-is-the-page		 * 
-		 * @param  string $getDelimiter [description]
+		 * @param  string $getDelimiter 
 		 * @return cString formatted-url-like-this
 		 */
 		public function toURL($getDelimiter = '-')
@@ -87,5 +120,55 @@
 			$str = $this->toASCII("'", $getDelimiter);
 			return new cString($str);
 		}
+		/**
+		 * truncate a string by length
+		 * if possible > the string is truncated on a whole word
+		 */
+		public function truncate($getIntLength, $getAppend='&hellip;')
+		{
+			$str = $this;
+			if (strlen($str) < $getIntLength) 
+				return $this;
+			$arrWords 	= explode(' ', $str);
+			$strNew 	= null;
+			foreach($arrWords as $strWord) {
+				// if first word is longer then $getIntLength
+				// cut the word at passed length 
+				if(
+					strlen($strWord) > $getIntLength &&
+					$strNew == null
+				) {
+					return new cString(substr($strWord, 0, $getIntLength).$getAppend);
+				}
+				// cut at last word
+				if ((strlen($strNew) + strlen($strWord)) > $getIntLength) {
+					return new cString($strNew.$getAppend);
+				}
+				$strNew .= (($strNew)? ' ' : '' ).$strWord;
+			}
+			// no need to truncate
+			return new cString($strNew);
+		}
+		/**
+		 * Encode string
+		 * @return cString encoded
+		 */
+		public function encode()
+		{
+			$str = $this
+				->gzdeflate()
+				->rot13()
+				->base64encode();
+			return new cString($str);				
+		}
+		public function decode()
+		{
+			$str = $this
+				->base64decode()
+				->rot13()
+				->gzinflate();
+			return new cString($str);
+		}
+
 	}
 ?>
